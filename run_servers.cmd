198a96417ba9f5d485765690e81335e74b4c7c45
Aman Kumar Thakur <160416669+ThakurAmanKumar@users.noreply.github.com>
2026-05-02 16:59:03 +0530

    Delete run_servers.cmd

--- run_servers.cmd
+++
@@ -1,2 +0,0 @@
-start powershell -NoExit -Command "cd 'C:\Users\LENOVO\Downloads\SmartMess\backend'; npm install; npm run dev"
-start powershell -NoExit -Command "cd 'C:\Users\LENOVO\Downloads\SmartMess\frontend'; npm install; npm run dev"
